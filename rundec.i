c846ab5978b4493b5e0aa9684590ba7b9c841a1e
David Straub <david.straub@tum.de>
2017-11-28 15:26:36 +0100

    Add Python wrapper

---
+++ rundec.i
@@ -0,0 +1,8 @@
+%module rundec
+
+%{
+#include <stdlib.h>
+#include "CRunDec3/CRunDec.h"
+%}
+
+%include "CRunDec3/CRunDec.h"
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension, find_packages
+
+ext_mod = Extension("_rundec",
+                    sources=["rundec.i", "CRunDec3/CRunDec.cpp"],
+                    swig_opts=['-c++'],
+                    )
+
+setup(name="rundec",
+      version="0.0.1",
+      author="David M. Straub",
+      author_email="david.straub@tum.de",
+      url="https://github.com/DavidMStraub/rundec-python",
+      description="A Python wrapper around the CRunDec package for the "
+                  " running and decoupling of the strong coupling constant "
+                  "and quark masses",
+      long_description="""`CRunDec` is a C++ program developed by
+      Florian Herren and Matthias Steinhauser. This Python package
+      provides a thin wrapper around `CRunDec` generated with SWIG.""",
+      license="GPLv3",
+      packages=find_packages(),
+      ext_modules=[ext_mod],
+      )
