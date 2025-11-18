2766ad7ce3c7f03fddaa90a4075af5180c793131
David Straub <straub@protonmail.com>
2025-11-18 14:42:53 +0100

    Use Python Stable ABI in Swig build

--- rundec.i
+++ rundec.i
@@ -1,5 +1,9 @@
 %module rundec
 
+%begin %{
+#define Py_LIMITED_API 0x03050000
+%}
+
 %{
 #include <stdlib.h>
 #include <utility>
--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, Extension
 
 ext_mod = Extension("_rundec",
                     sources=["_rundec.cc", "CRunDec3/CRunDec.3.1.cpp"],
+                    define_macros=[('Py_LIMITED_API', '0x03050000')],
+                    py_limited_api=True,
                     )
 
 setup(name="rundec",
-      version="0.6",
+      version="0.7",
       author="David M. Straub",
-      author_email="david.straub@tum.de",
+      author_email="david.straub@hm.edu",
       url="https://github.com/DavidMStraub/rundec-python",
       description="A Python wrapper around the CRunDec package for the "
                   " running and decoupling of the strong coupling constant "
